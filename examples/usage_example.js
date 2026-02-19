c8b789b3ed7653c2525d7d02d274f962cf197861
Gabriel Demetrios Lafis <33261275+galafis@users.noreply.github.com>
2026-02-19 16:34:34 -0300

    Aprimorar qualidade do projeto: package.json, exemplos de uso

---
+++ examples/usage_example.js
@@ -0,0 +1,22 @@
+/**
+ * Exemplo de uso - Criando-uma-Minimal-API-da-Formula-1-com-Node.js-e-Fastify
+ *
+ * Demonstra as principais funcionalidades do projeto.
+ */
+
+// Importar modulos principais
+// const { driverController } = require('./src/controllers/driverController');
+// const { drivers } = require('./src/database/drivers');
+
+async function main() {
+  // Inicializar driverController
+  // const instance = new driverController();
+  console.log("driverController carregado com sucesso");
+
+  // Inicializar drivers
+  // const instance = new drivers();
+  console.log("drivers carregado com sucesso");
+
+}
+
+main().catch(console.error);
